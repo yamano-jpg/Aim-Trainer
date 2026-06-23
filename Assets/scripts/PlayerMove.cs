501d72947d584301b728e7c2be7003f77cf18208
yamano <ysouta2025@docomo.ne.jp>
2026-06-23 18:42:05 +0900

    初回コミット

---
+++ Assets/scripts/PlayerMove.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 5f;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
